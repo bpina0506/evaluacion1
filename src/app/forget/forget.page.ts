1c06e79fefab478dd557dedb62e0b5d4c3477577
Rodrigo Gonzáledz <rod.gonzalezv@duocuc.cl>
2023-09-07 19:28:14 -0300

    ultimos cambios, boton back

--- src/app/forget/forget.page.ts
+++ src/app/forget/forget.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CheckboxCustomEvent } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';  //genera interpolacion
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forget',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget.page.scss'],
 })
 export class ForgetPage implements OnInit {
+  canDismiss = false;
 
-  constructor() { }
+  presentingElement: Element | null = null;
+  
+
+  constructor(private router: Router) { } // Se debe instanciar
 
   ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
   }
+
+  olvidaPass(){
+    this.router.navigate(['/login']); 
+  }
+
+
+  onTermsChanged(event: Event) {
+    const ev = event as CheckboxCustomEvent;
+    this.canDismiss = ev.detail.checked;
+  }
+
 
 }
